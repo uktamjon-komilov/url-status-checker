3cfbac517e6b7121420f8b5b09dcd58ec2190176
Uktamjon <uktamforjob@gmail.com>
2021-07-01 00:08:04 +0500

    Backend is working

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.list_of_urls, name = "list_of_urls"),
     path("create/", views.add_url, name = "add_url"),
-    path("update/", views.update_url, name = "update_url")
+    path("update/<int:_id>", views.update_url, name = "update_url")
 ]
--- app/views.py
+++ app/views.py
@@ -1,15 +1,79 @@
-from django.shortcuts import render
+from django.conf.urls import url
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from .models import Url
 
 def list_of_urls(request):
     context = {}
+    if request.GET.get("action", None) == "delete":
+        try:
+            _id = int(request.GET.get("id"))
+            urls = Url.objects.filter(id=_id)
+            if urls.exists():
+                urls.delete()
+            return redirect("list_of_urls")
+        except:
+            return redirect("list_of_urls")
+
+    urls = Url.objects.filter(user=request.user).all()
+    context["urls"] = urls
     return render(request, "service/list.html", context)
 
 
 def add_url(request):
-    context = {}
-    return render(request, "service/create.html", context)
+    if request.method == "POST":
+        url_obj = Url()
+
+        title = request.POST.get("title", None)
+        if title:
+            url_obj.custom_name = title
+        
+        url = request.POST.get("url", None)
+        if url:
+            url_obj.url = url
+        
+        interval = request.POST.get("request-interval", None)
+        if interval:
+            url_obj.request_interval = int(interval)
+        
+        url_obj.user = request.user
+        url_obj.save()
+
+        messages.add_message(request, messages.SUCCESS, "You have added a new link to be observed!", extra_tags="text text-success")
+
+        return redirect("list_of_urls")
+
+    return render(request, "service/create.html")
 
 
-def update_url(request):
+def update_url(request, _id):
     context = {}
+
+    urls = Url.objects.filter(id = _id)
+    if urls.exists():
+        obj = urls.first()
+        context["obj"] = obj
+    else:
+        return redirect("list_of_urls")
+
+    if request.method == "POST":
+        title = request.POST.get("title", None)
+        if title:
+            obj.custom_name = title
+        
+        url = request.POST.get("url", None)
+        if url:
+            obj.url = url
+        
+        interval = request.POST.get("request-interval", None)
+        if interval:
+            obj.request_interval = int(interval)
+        
+        obj.user = request.user
+        obj.save()
+
+        messages.add_message(request, messages.SUCCESS, "You have updated a link!", extra_tags="text text-success")
+
+        return redirect("list_of_urls")
+
     return render(request, "service/update.html", context)
