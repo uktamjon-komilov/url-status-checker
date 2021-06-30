ace8944cd4571bc115d7e53266dec840fc733400
Uktamjon <uktamforjob@gmail.com>
2021-06-30 22:16:01 +0500

    Created basic url model which might be enough for solving the problem

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
+from account.models import User
 from django.db import models
 
-# Create your models here.
+class Url(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    custom_name = models.CharField(max_length=255, blank=True)
+    url = models.TextField()
+    request_interval = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{}: {}".format(self.user.fullname, self.url)
